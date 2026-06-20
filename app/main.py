05412b4078224d588a5f7351023f4e2383dd69ce
Andrey Jekul <andrey.jekul@rhinocodes.com>
2026-06-20 16:18:15 +0300

    Added cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,24 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+    f_name = func.__name__
+
+    if not cache_data.get("f_name", False):
+        cache_data[f_name] = {}
+
+    cur_cache = cache_data[f_name]
+
+    def inner(*args) -> Callable:
+
+        result = cur_cache.get(args, False)
+        if result is not False:
+            print("Getting from cache")
+            return result
+
+        result = func(*args)
+        cur_cache[args] = result
+        print("Calculating new result")
+        return result
+
+    return inner
